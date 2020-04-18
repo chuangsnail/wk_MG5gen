ccc20ea1f9c1c920009fa4622c3b57bed4929b70
cychuang_莊振宇 <jeff0118252718@gmail.com>
2020-04-18 23:59:58 +0800

    add scripts of exe_TP_semi*

---
+++ scr_exe/exe_TP_semi_GVSel.sh
@@ -0,0 +1,17 @@
+#! bin/bash
+
+cd /wk_cms2/cychuang/CMSSW_9_4_13/src/wk_MG5gen/ && eval `scramv1 runtime -sh`
+
+TP_semilep 10_6/semi_SM_1j GVSel
+
+TP_semilep 10_6/semi_CEDM0pt5_1j GVSel
+TP_semilep 10_6/semi_CEDM1_1j GVSel
+TP_semilep 10_6/semi_CEDM1pt5_1j GVSel
+TP_semilep 10_6/semi_CEDM2_1j GVSel
+TP_semilep 10_6/semi_CEDM2pt5_1j GVSel
+TP_semilep 10_6/semi_CEDM3_1j GVSel
+TP_semilep 10_6/semi_CEDM3pt5_1j GVSel
+TP_semilep 10_6/semi_CEDM4_1j GVSel
+TP_semilep 10_6/semi_CEDM4pt5_1j GVSel
+TP_semilep 10_6/semi_CEDM5_1j GVSel
+
